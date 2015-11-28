7030dae209e0f9929bdcf06dbdb46d6d48644fa9
Alexander L <1214160@gmail.com>
2015-11-28 15:21:37 +0500

    Unworkable parser preparations deleted. 	modified:   src/parser.c

--- src/parser.c
+++ src/parser.c
@@ -22,32 +22,6 @@
 #include "proc.h"
 
 
-enum inp_char {
-	IC_DIG, IC_PNT, IC_ALP, IC_SEM, IC_SPC
-};
-
-enum state {
-	S_NUMNAM, S_NUM, S_NUMSPC, S_PNT, S_NAM, S_NAMSPC, S_A1, S_A1SPC, S_SEP, S_SEPSPC, S_A2, S_HALT
-};
-
-
-static int state_table[][5] = {
-/*   IC_DIG    IC_PNT    IC_ALP    IC_SEM    IC_SPC
-     0-9       .         A-Z       ;         space */
-	{S_NUM,    S_HALT,   S_NAM,    S_HALT,   S_NUMNAM}, /* S_NUMNAM -- command number or command name */
-	{S_NUM,    S_PNT,    S_HALT,   S_HALT,   S_NUMSPC}, /* S_NUM    -- command number */
-	{S_HALT,   S_PNT,    S_HALT,   S_HALT,   S_NUMSPC}, /* S_NUMSPC -- space after command number */
-	{S_HALT,   S_HALT,   S_NAM,    S_HALT,   S_PNT},    /* S_PNT    -- point after command number */
-	{S_A1,     S_HALT,   S_NAM,    S_HALT,   S_NAMSPC}, /* S_NAM    -- command name */
-	{S_A1,     S_HALT,   S_HALT,   S_HALT,   S_NAMSPC}, /* S_NAMSPC -- space after command name */
-	{S_A1,     S_HALT,   S_HALT,   S_SEP,    S_A1SPC},  /* S_A1     -- address 1 */
-	{S_HALT,   S_HALT,   S_HALT,   S_SEP,    S_A1SPC},  /* S_A1SPC  -- space after address 1 */
-	{S_A2,     S_HALT,   S_HALT,   S_HALT,   S_SEPSPC}, /* S_SEP    -- address separator */
-	{S_A2,     S_HALT,   S_HALT,   S_HALT,   S_SEPSPC}, /* S_SEPSPC -- space after address separator */
-	{S_A2,     S_HALT,   S_HALT,   S_HALT,   S_HALT}    /* S_A2     -- address 2 */
-};
-
-
 static void uncomm(char *s)
 {
 	if (!s) return;
@@ -64,38 +38,10 @@ static void uncomm(char *s)
 excode_t prs_progline(char *s)
 {
 	int a1, a2;
-	char *c;
-	int state = 0;
-	enum inp_char ic;
 	opcode_t op;
 	a1 = a2 = 0;
 	uncomm(s);
 	if (!*s) return E_SUCC;
-
-	/*  TO-DO: normal parsing
-	c = s;
-	while (!c) {
-		if (isdigit(*c)) {
-			ic = IC_DIG;
-		} else if (*c == '.') {
-			ic = IC_PNT;
-		} else if (isalpha(*c)) {
-			ic = IC_ALP;
-		} else if (*c == ';') {
-			ic = IC_SEM;
-		} else if (isspace(*c)) {
-			ic = IC_SPC;
-		} else {
-			return E_PARSE;
-		}
-		state = state_table[state][ic];
-		printf("state: %d\n", state);
-		c++;
-	}
-	*/
-
-
-
 	switch (s[0]) {
 	case 'V':
 		if (sscanf(s+1, "%d", &a1) != 1) return E_PARSE;
